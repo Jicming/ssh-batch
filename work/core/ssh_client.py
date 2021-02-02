f41efe3532f243624454de00a0b010d9db0dd2c0
jack ji <varshadow@163.com>
2021-02-02 10:50:55 +0800

    third commit

--- work/core/ssh_client.py
+++ work/core/ssh_client.py
@@ -10,16 +10,24 @@ class SSH_link(object):
         self.password = host_config.get('password')
         self.port = host_config.get('port')
         self.ssh = paramiko.SSHClient()
-        self.SSH_Connect()
+
     def SSH_Connect(self):
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh.connect(hostname=self.hostname,port=self.port,username=self.username,password=self.password)
-    def SSH_Execute(self,commond):
-        stdin,stdout,stderr = self.ssh.exec_command(commond)
-        res,err = stdout.read(),stderr.read()
-        result = res if res else err
 
-        print('--------------------- %s(%s) -------------'%(self.title,self.hostname))
-        print(result.decode('utf8'))
+    def SSH_Execute(self,commond):
+        try:
+            self.SSH_Connect()
+            stdin, stdout, stderr = self.ssh.exec_command(commond)
+            res, err = stdout.read(), stderr.read()
+            result = res if res else err
+            print('--------------------- %s(%s) -------------' % (self.title, self.hostname))
+            print(result.decode('utf8'))
+        except TimeoutError as f:
+            print('--------------------- %s(%s) -------------' % (self.title, self.hostname))
+            print('%s:%s 连接尝试失败'%(self.title,self.hostname))
+
+
+
 
 
